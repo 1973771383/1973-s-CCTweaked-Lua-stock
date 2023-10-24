bd769984a065161f31f0ffaa78aa546e17a0aca0
1973771383 <143469055+1973771383@users.noreply.github.com>
2023-10-24 11:40:17 +0800

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,28 +1,29 @@
-RSC = peripheral.find("Create_RotationSpeedController")
-SOM = peripheral.find("Create_Stressometer")
- 
-if term.isColour() then 
-    shell.run("bg")
-    term.setTextColour(colors.orange)
-    print("Auto-Speed-Regulation activated")
-else print("Auto-Speed-Regulation activated")
+RSC = peripheral.find("Create_RotationSpeedController")
+SOM = peripheral.find("Create_Stressometer")
+ 
+if term.isColour() then 
+    shell.run("bg")
+    term.setTextColour(colors.orange)
+    print("Auto-Speed-Regulation activated")
+else print("Auto-Speed-Regulation activated")
 end
- 
-function Main()
-    CurStress = SOM.getStress()
-    TotalStress  = SOM.getStressCapacity()
-    CurSpeed = RSC.getTargetSpeed()
-    if(CurStress ~= TotalStress) then
-        DeltaStressMult = CurStress/TotalStress
-        PN = CurSpeed/math.abs(CurSpeed)
-        Abs_TargetSpeed = math.abs(CurSpeed/DeltaStressMult)
-        if Abs_TargetSpeed > 256 then Abs_TargetSpeed = 256
-        -- elseif Abs_TargetSpeed < 32 then Abs_TargetSpeed = 32        
-        end
-        RSC.setTargetSpeed(Abs_TargetSpeed * PN)
-    end
+ 
+function Main()
+    CurStress = SOM.getStress()
+    TotalStress  = SOM.getStressCapacity()
+    CurSpeed = RSC.getTargetSpeed()
+    if(CurStress ~= TotalStress) then
+        DeltaStressMult = CurStress/TotalStress
+        PN = CurSpeed/math.abs(CurSpeed)
+        Abs_TargetSpeed = math.abs(CurSpeed/DeltaStressMult)
+        if Abs_TargetSpeed > 256 then Abs_TargetSpeed = 256
+        -- elseif Abs_TargetSpeed < 32 then Abs_TargetSpeed = 32        
+        end
+        RSC.setTargetSpeed(Abs_TargetSpeed * PN)
+    end
 end
- 
+ 
 repeat
-    Main()
-    sleep(0.5)
+    Main()
+    sleep(0.5)
+until false
